4eb9ee513247a177aeb4d023d35391f465a0e52f
Tuxraldo <127690477+Tuxraldo@users.noreply.github.com>
2023-10-31 20:24:09 -0300

    .

--- st/config.def.h
+++ st/config.def.h
@@ -112,36 +112,6 @@ float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 #include </home/tuxraldo/.dwm/st-themes/dark_pastels.h>
-//static const char *colorname[] = {
-//	/* 8 normal colors */
-//	"black",
-//	"red3",
-//	"green3",
-//	"yellow3",
-//	"blue2",
-//	"magenta3",
-//	"cyan3",
-//	"gray90",
-//
-//	/* 8 bright colors */
-//	"gray50",
-//	"red",
-//	"green",
-//	"yellow",
-//	"#5c5cff",
-//	"magenta",
-//	"cyan",
-//	"white",
-//
-//	[255] = 0,
-//
-//	/* more colors can be added after 255 to use with DefaultXX */
-//	"#cccccc",
-//	"#555555",
-//	"gray90", /* default foreground colour */
-//	"black", /* default background colour */
-//};
-
 
 /*
  * Default colors (colorname index)
